46d095ef116d4736bee28e0db15faf721d4e4f77
Chris Quattrocchi and Kevin Boyle <chris@>
2017-10-31 16:38:38 -0700

    set default 0 values for entry fields

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
 //back end Logic
 function AccountSetUp(name, deposit) {
-  if (isNaN(deposit)) {
-    alert("please enter a valid deposit number")
-  }
+
   this.accountname = name;
   this.initDeposit = deposit;
   AccountSetUp = 0 + deposit;
@@ -19,19 +17,22 @@ function Account(addNewDeposit, addNewWithdrawal) {
 $(document).ready(function(){
   $("#newAccount").submit(function(event){
     event.preventDefault();
-    $(".hideSetUp").hide();
-    $(".hideDeposit").show();
     var acctName = $(".initialName").val();
     var initialDeposit = parseInt($(".initialDeposit").val());
     var account = new AccountSetUp(acctName, initialDeposit);
-    $(".displayBalance").text("$" + AccountSetUp);
+    if ((initialDeposit !== NaN) && (acctName !== "")) {
+      $(".hideSetUp").hide();
+      $(".hideDeposit").show();
+      $(".displayBalance").text("$" + AccountSetUp);
+    }
 
-    $("#depositForm").submit(function(event){
-      event.preventDefault();
-      var addNewDeposit = parseInt($(".newDeposit").val());
-      var addNewWithdrawal = parseInt($(".newWithdrawal").val());
-      var newAccount = new Account(addNewDeposit, addNewWithdrawal);
-      $(".displayBalance").text("$" + Account);
+  });//newAcc function
+
+  $("#depositForm").submit(function(event){
+    event.preventDefault();
+    var addNewDeposit = parseInt($(".newDeposit").val());
+    var addNewWithdrawal = parseInt($(".newWithdrawal").val());
+    var newAccount = new Account(addNewDeposit, addNewWithdrawal);
+    $(".displayBalance").text("$" + Account);
     });//deposit function
-  })//newAcc function
 });//end doc rdy
